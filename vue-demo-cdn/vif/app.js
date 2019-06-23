2281b061f4e0d304f1b13bcbc697538998925e20
Jason-HuHu <1259450641@qq.com>
2019-06-23 10:27:41 +0800

    v-if 与 v-show的区别（无占位与有占位）

---
+++ vue-demo-cdn/vif/app.js
@@ -0,0 +1,27 @@
+new Vue({
+	el:'#app',
+	data:{
+		name:"小胡",
+		job:"Java工程师",
+		website:"http://www.baidu.com",
+
+		error:false,
+		success:false
+	},
+	//方法
+	methods:{
+		hello:function(){
+			return 'Good Morning'
+		},
+	},
+	//计算属性
+	computed:{
+		compClasses:function () {
+			return {
+				changeColor: this.changeColor,
+				changeLength: this.changeLength
+			}
+		}
+	}
+});
+
