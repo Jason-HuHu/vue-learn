754f3d99ef1e0df8341e8540ff71888117867fef
Jason-HuHu <1259450641@qq.com>
2019-06-26 22:21:33 +0800

    初始化路由项目

---
+++ vue-demo-cli3/src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button>mint-ui</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        methods:{
+            gotoMobile () {
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
